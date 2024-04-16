205027df5be060dab4bf59de96fe8fcb4c826291
AbdouBouallaga <babdelka@student.1337.ma>
2024-04-16 22:31:20 +0100

    clean up tha mess

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Start the containers
-# docker-compose -f ./ubuntu_container/docker-compose.yaml up -d
-
-# SSH into the container
-# docker exec -it Ubuntu_14.10 /bin/bash
-
-vagrant up
--- stop.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker-compose -f ./ubuntu_container/docker-compose.yaml down
